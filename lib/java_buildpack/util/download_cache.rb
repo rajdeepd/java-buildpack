e96ceb7caf40d346b8455d858180c06791933988
Glyn Normington <gnormington@gopivotal.com>
2013-10-30 09:24:49 +0000

    Support Errno::ENETUNREACH
    The internet availability check was failing because Errno::ENETUNREACH
    was not being handled.
    
    This fix adds Errno::ENETUNREACH and a number of other network-related
    exceptions to the set being handled. When these errors are caught on
    the first download attempt in a buildpack invocation (detect, compile,
    or release), the internet is deemed to be inaccessible and a look-
    aside is performed in the (currently read-only, but pre-populated)
    buildpack cache.
    
    See https://www.pivotaltracker.com/s/projects/803319/stories/58941750
    for background and https://www.pivotaltracker.com/story/show/59817096
    for the corresponding bug fix.

--- lib/java_buildpack/util/download_cache.rb
+++ lib/java_buildpack/util/download_cache.rb
@@ -95,10 +95,17 @@ module JavaBuildpack::Util
 
     HTTP_ERRORS = [
         EOFError,
+        Errno::ECONNABORTED,
         Errno::ECONNREFUSED,
         Errno::ECONNRESET,
+        Errno::EHOSTDOWN,
         Errno::EHOSTUNREACH,
         Errno::EINVAL,
+        Errno::ENETDOWN,
+        Errno::ENETRESET,
+        Errno::ENETUNREACH,
+        Errno::ENONET,
+        Errno::ENOTCONN,
         Errno::EPIPE,
         Errno::ETIMEDOUT,
         Net::HTTPBadResponse,
